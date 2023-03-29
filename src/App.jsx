98e59c2d7d2ac84681498480be2eff922c4635d7
td2thinh <75013885+td2thinh@users.noreply.github.com>
2023-03-30 01:15:36 +0200

    Added Responsive Grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Box, Container } from "@mui/material";
+import { Box, Container, useMediaQuery } from "@mui/material";
 import Gauge from './components/Gauge';
 import Client from './components/Client';
 
@@ -10,23 +10,58 @@ const gridTemplate = `
   "gauge client"
   "map client"
   "map client"
-  "map client"
+  "map stats"
   "map stats"
   "map stats"
   "map stats"
   "map stats"
   "map stats"
 `
-function App() {
+
+const gridTemplateSmallScreen = `
+  "gauge"
+  "gauge"
+  "gauge"
+  "gauge"
+  "client"
+  "client"
+  "client"
+  "client"
+  "client"
+  "client"
+  "client"
+  "map"
+  "map"
+  "map"
+  "map"
+  "map"
+  "map"
+  "map"
+  "map"
+  "stats"
+  "stats"
+  "stats"
+  "stats"
+  "stats"
+`
+
+const App = () => {
+  const isBigScreen = useMediaQuery('(min-width: 1200px)');
   return (
     <Container sx={{ padding: 10 }}>
       <Box width="100%" height="100%" display="grid"
         gap="1.5rem"
-        sx={{
-          gridTemplate: gridTemplate,
-          gridTemplateColumns: "repeat(2, 1fr)",
-          gridTemplateRows: "repeat(2, 1fr)",
-        }}>
+        sx={
+          isBigScreen ? {
+            gridTemplate: gridTemplate,
+            gridTemplateColumns: "repeat(2, 1fr)",
+            gridTemplateRows: "repeat(2, 1fr)",
+          }
+            : {
+              gridTemplate: gridTemplateSmallScreen,
+              gridAutoColumns: "1fr",
+              gridAutoRows: "1fr",
+            }}>
         <Box sx={{ gridArea: "gauge" }}>
           <Gauge />
         </Box>
