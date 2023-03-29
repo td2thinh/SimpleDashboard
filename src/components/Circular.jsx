cbd7fdeb093be4e08d308bd5d83da2f2808eed3e
td2thinh <75013885+td2thinh@users.noreply.github.com>
2023-03-30 00:00:18 +0200

    Done client Paper

--- src/components/Circular.jsx
+++ src/components/Circular.jsx
@@ -1,7 +1,6 @@
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import { max } from 'd3';
 
 
 const Circular = (props) => {
--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,21 +1,46 @@
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import React from 'react';
+import LocalShipping from '@mui/icons-material/LocalShipping';
+import ZoomIn from '@mui/icons-material/ZoomIn';
+import IconButton from '@mui/material/IconButton';
 
 const Client = () => {
     return (
         <>
-            <Paper elevation={3}>
+            <Paper elevation={3} sx={{ maxHeight: 400, overflow: 'auto' }} >
                 <Typography padding={2} sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    CLIENT
+                    CLIENTS
                 </Typography>
-                <Grid container>
-                    <Grid item xs={12} md={6}>
-                        <Typography padding={2} sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                            Nom
-                        </Typography>
-                    </Grid>
-                </Grid>
+                <List>
+                    {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((value) => {
+                        return (
+                            <Grid container container direction="row" alignItems="center">
+                                <ListItem
+                                    secondaryAction={
+                                        <Grid container container direction="row" alignItems="center">
+                                            <Typography sx={{ fontSize: 15 }}>
+                                                19%
+                                            </Typography>
+                                            <IconButton edge="end">
+                                                <ZoomIn />
+                                            </IconButton>
+                                            <IconButton edge="end">
+                                                <LocalShipping />
+                                            </IconButton>
+                                        </Grid>
+                                    }
+                                >
+                                    <ListItemText primary={`Client ${value}`} />
+                                </ListItem>
+                            </Grid>
+                        )
+                    })}
+                </List>
             </Paper>
         </>
     )
